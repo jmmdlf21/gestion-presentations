829bc7ef6c29514e1b0e05c3c8cb3968eb627517
Josemon <jmmdlf21@gmail.com>
2020-03-01 23:56:36 -0500

    css update

--- client/js/main.js
+++ client/js/main.js
@@ -183,7 +183,7 @@ let gestionPresentations = function() {
 						// Pour ne montrer le contenu qu'une fois que le tout a chargé	
 						$("#wrapper").addClass("displayNone");
 
-						let html  =	"<h3 class='thematique'>" + theme + "</h3>";
+						let html  =	"<h3 class='thematique'>"+theme+"</h3>";
 						html     += "<div class='presentation accordion2'>";
 						html     += "<ul class='sortable'>";
 
@@ -208,16 +208,16 @@ let gestionPresentations = function() {
 										htmlMess += "<li>";
 										htmlMess +=   "<h4>" + c.titre + "</h4>";
 										htmlMess +=   "<div class='presentation'>";
+										htmlMess +=     "<h5>"+c.resume+"</h5>";																					
 										htmlMess +=     "<h5><span>Presenté le </span>"+c.date+"</h5>";										
-										htmlMess +=     "<h6>"+c.resume+"</h6>";
-										htmlMess +=     "<span class='modifier' data-action='modifier'";  
-										htmlMess +=     "data-id='"+c.id_presentation+"'>Modifier</span>";
-										htmlMess +=     "<span class='supprimer' data-action='supprimer'"; 
-										htmlMess +=     "data-id='"+c.id_presentation+"'>Supprimer</span>";
 										htmlMess +=     "<h5><span>De </span>"+c.heure_debut;
 										htmlMess +=     "<span> À </span>"+c.heure_fin+"</h5>";	
 										htmlMess +=     "<h5><span>Presenté par </span>"+c.presentateur+"</h5>";										
 										htmlMess +=     "<h5><span>Salle </span>"+ c.salle+"</h5>";
+										htmlMess +=     "<span class='modifier' data-action='modifier'";  
+										htmlMess +=     "data-id='"+c.id_presentation+"'>Modifier</span>";
+										htmlMess +=     "<span class='supprimer' data-action='supprimer'"; 
+										htmlMess +=     "data-id='"+c.id_presentation+"'>Supprimer</span>";																			
 										htmlMess +=   "</div>";
 										htmlMess += "</li>";
 									});
@@ -235,6 +235,7 @@ let gestionPresentations = function() {
 						if($("#accordionThematiques").children().length == themes.length * 2)
 							resolve();
 					}, 500);
+
 				}).catch(() => {
 					$("#wrapper").html("");
 					$("#erreur").html("Erreur lors du chargement du contenu");	
